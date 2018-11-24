f0cd361c29995dd72e64a61e1db2367fd7657f3d
Hunter Caron <huntercaron@me.com>
2018-11-24 16:47:46 +0100

    making about

---
+++ src/templates/about.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import { graphql } from 'gatsby'
+import get from 'lodash/get'
+
+import Layout from '../components/layout'
+
+class AboutTemplate extends React.Component {
+  render() {
+    const post = this.props.data.markdownRemark
+    const siteTitle = get(this.props, 'data.site.siteMetadata.title')
+    const siteDescription = post.excerpt
+    // const { previous, next } = this.props.pageContext
+
+    return (
+      <Layout location={this.props.location} pageTitle={post.frontmatter.title}>
+        <Helmet
+          htmlAttributes={{ lang: 'en' }}
+          meta={[{ name: 'description', content: siteDescription }]}
+          title={`${post.frontmatter.title} | ${siteTitle}`}
+        />
+
+        <h1>TEST</h1>
+        {post.frontmatter.color}
+      </Layout>
+    )
+  }
+}
+
+export default AboutTemplate
+
+export const pageQuery = graphql`
+  query AboutPage($slug: String!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      excerpt
+      html
+      frontmatter {
+        title
+        color
+      }
+    }
+  }
+`
