e80c61d211c2a5dbc28a3cdeb5e290236a954be5
Hunter Caron <huntercaron@me.com>
2020-04-23 10:47:59 +0200

    Updating packages

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,30 +5,64 @@ import Layout from '../components/layout'
 import PageLink from '../components/pageLink'
 
 const SectionTitle = ({ title }) => (
-  <h2 style={{fontSize: "4rem", marginTop: "4rem"}}>
-    {title}
-  </h2>
+  <h2 style={{ fontSize: '36px', marginTop: '4rem' }}>{title}</h2>
 )
 
-const IndexPage = ({ data: { projects: { edges: projects }, blog: { edges: posts }}}) => (
+const IndexPage = ({
+  data: {
+    projects: { edges: projects },
+    blog: { edges: posts },
+  },
+}) => (
   <Layout>
-    <h1 style={{maxWidth: "500px"}}>A <a href="https://www.gatsbyjs.org/" target="_blank" rel="noopener noreferrer">Gatsby</a> boilerplate using <a href="https://forestry.io/" target="_blank" rel="noopener noreferrer">Forestry</a> as a headless CMS.</h1>
-    <h1 style={{maxWidth: "500px"}}>Find it on <a href="https://github.com/huntercaron/sunnyside" target="_blank" rel="noopener noreferrer">github</a>.</h1>
+    <h1 style={{ maxWidth: '450px' }}>
+      A{' '}
+      <a
+        href="https://www.gatsbyjs.org/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Gatsby
+      </a>{' '}
+      boilerplate using{' '}
+      <a href="https://forestry.io/" target="_blank" rel="noopener noreferrer">
+        Forestry
+      </a>{' '}
+      as a CMS.
+    </h1>
+    <h1 style={{ maxWidth: '500px' }}>
+      Find it on{' '}
+      <a
+        href="https://github.com/huntercaron/sunnyside"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        github
+      </a>
+      .
+    </h1>
 
-    <SectionTitle title="Projects"/>
-    {projects.map(({ node: project }) => 
-      <PageLink key={project.id} link={project.fields.slug} title={project.frontmatter.title}/>
-    )}
+    <SectionTitle title="Projects" />
+    {projects.map(({ node: project }) => (
+      <PageLink
+        key={project.id}
+        link={project.fields.slug}
+        title={project.frontmatter.title}
+      />
+    ))}
 
-    <SectionTitle title="Blog"/>
-    {posts.map(({ node: post }) => 
-      <PageLink key={post.id} link={post.fields.slug} title={post.frontmatter.title}/>
-    )}
+    <SectionTitle title="Blog" />
+    {posts.map(({ node: post }) => (
+      <PageLink
+        key={post.id}
+        link={post.fields.slug}
+        title={post.frontmatter.title}
+      />
+    ))}
 
     <Link to="/about">
-      <SectionTitle title="About &rarr;"/>
+      <SectionTitle title="About &rarr;" />
     </Link>
-
   </Layout>
 )
 
@@ -36,7 +70,9 @@ export default IndexPage
 
 export const query = graphql`
   query IndexQuery {
-    projects: allMarkdownRemark(filter: { fields: { slug: { regex: "/projects/" }}}) {
+    projects: allMarkdownRemark(
+      filter: { fields: { slug: { regex: "/projects/" } } }
+    ) {
       edges {
         node {
           id
@@ -50,7 +86,9 @@ export const query = graphql`
       }
     }
 
-    blog: allMarkdownRemark(filter: { fields: { slug: { regex: "/posts/" }}}) {
+    blog: allMarkdownRemark(
+      filter: { fields: { slug: { regex: "/posts/" } } }
+    ) {
       edges {
         node {
           id
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,33 +1,28 @@
 import React from 'react'
-import Helmet from 'react-helmet'
+import { Helmet } from 'react-helmet'
 import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 
-class BlogPostTemplate extends React.Component {
-  render() {
-    const post = this.props.data.markdownRemark
-    const siteTitle = this.props.data.site.siteMetadata.title
-    const siteDescription = post.excerpt
+export default function BlogPostTemplate(props) {
+  const post = props.data.markdownRemark
+  const siteTitle = props.data.site.siteMetadata.title
 
-    return (
-      <Layout location={this.props.location} pageTitle={post.frontmatter.title}>
-        <Helmet
-          htmlAttributes={{ lang: 'en' }}
-          meta={[{ name: 'description', content: siteDescription }]}
-          title={`${post.frontmatter.title} | ${siteTitle}`}
-        />
-        <div 
-          dangerouslySetInnerHTML={{ __html: post.html }} 
-          style={{fontSize: "2rem", margin: "0 3rem", maxWidth: "700px"}}
-        />
-        
-      </Layout>
-    )
-  }
+  return (
+    <Layout location={props.location} pageTitle={post.frontmatter.title}>
+      <Helmet>
+        <title>
+          {post.frontmatter.title} | {siteTitle}
+        </title>
+      </Helmet>
+
+      <div
+        dangerouslySetInnerHTML={{ __html: post.html }}
+        style={{ fontSize: '2rem', margin: '0 3rem', maxWidth: '700px' }}
+      />
+    </Layout>
+  )
 }
-
-export default BlogPostTemplate
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
