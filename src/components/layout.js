834c42476d885d39778912dfdd8b9b015eb36852
Hunter Caron <huntercaron@me.com>
2020-06-04 15:16:16 +0200

    Attempt 3

--- src/components/layout.js
+++ src/components/layout.js
@@ -23,7 +23,8 @@ const Layout = ({ children, pageTitle }) => {
         // Cancel the event
         e.preventDefault() // If you prevent default behavior in Mozilla Firefox prompt will always be shown
         // Chrome requires returnValue to be set
-        e.returnValue = ""
+        // e.returnValue = ""
+        return
       }
     }
 
